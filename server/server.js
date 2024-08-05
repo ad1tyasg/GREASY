c5a75e07f5ffc2bc535ad2d598128a4ce01585fb
ad1tyasg <adityasg152003@gmail.com>
2024-08-05 14:54:34 +0530

    first commit

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const app = express()
 const db = require('./db/conn')
 app.use(cors());
 app.use(express.json())
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.get('/record', require(path.join(__dirname, 'routes/records.js')))
 app.get('/quiz', require(path.join(__dirname, 'routes/records.js')))
